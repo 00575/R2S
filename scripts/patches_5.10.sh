a770e7da29c1c24bf3947e5a471cd8d83a3cf0dc
a0575 <68583179+a0575@users.noreply.github.com>
2022-06-12 07:10:24 +0800

    Update patches_5.10.sh

--- scripts/patches_5.10.sh
+++ scripts/patches_5.10.sh
@@ -29,6 +29,7 @@ sed -i 's/5.0/1.0/' .ccache/ccache.conf || true
 line_number_INCLUDE_V2ray=$[`grep -m1 -n 'Include V2ray' package/custom/openwrt-passwall/luci-app-passwall/Makefile|cut -d: -f1`-1]sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-passwall/Makefile
 sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-passwall/Makefile
 sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-passwall/Makefile
+sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-passwall/Makefile
 
 if [ $BRANCH == 'master' ]; then
 
