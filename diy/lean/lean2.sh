b5f3d8ef8bba3a184d838a95ddbcf0769fee5d8c
00575 <75769819+00575@users.noreply.github.com>
2022-09-03 11:23:39 +0800

    Update lean2.sh

--- diy/lean/lean2.sh
+++ diy/lean/lean2.sh
@@ -12,7 +12,7 @@ echo -e "msgstr \"日志\"" >> package/feeds/luci/luci-app-dnsforwarder/po/zh-cn
               
 ##配置ip等
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 sed -i '/root::0:0:99999/d' package/lean/default-settings/files/zzz-default-settings
 
