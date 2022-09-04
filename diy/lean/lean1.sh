9d81066e8be74a8999f6f78e2604d32493ef44fd
00575 <75769819+00575@users.noreply.github.com>
2022-09-04 19:01:55 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -4,5 +4,5 @@
 rm -rf ./package/lean/k3screenctrl
 rm -rf ./package/lean/autocore
                
-sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" feeds.conf.default
+sed -i "1isrc-git 00575 https://github.com/00575/openwrt-packages" feeds.conf.default
 sed -i "2isrc-git ipkg https://github.com/xiangfeidexiaohuo/op-ipkg\n" feeds.conf.default
