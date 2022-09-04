353f73a72a096befa6a8162f28607570ea7d396f
00575 <75769819+00575@users.noreply.github.com>
2022-09-04 09:42:48 +0800

    Update lean2.sh

--- diy/lean/lean2.sh
+++ diy/lean/lean2.sh
@@ -24,11 +24,19 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/
 
 ##加入作者信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='$(date +%Y%m%d)'/g" package/lean/default-settings/files/zzz-default-settings   
-#sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' YaoDao'/g" package/lean/default-settings/files/zzz-default-settings
-#cp -af feeds/xiangfeidexiaohuo/patch/banner  package/base-files/files/etc/
+# sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' YaoDao'/g" package/lean/default-settings/files/zzz-default-settings
+# cp -af feeds/xiangfeidexiaohuo/patch/banner  package/base-files/files/etc/
+
+## add pwm fan control service
+wget https://github.com/friendlyarm/friendlywrt/commit/cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+git apply cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+rm cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+sed -i 's/pwmchip1/pwmchip0/' target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
+
 
 ##更改主机名
 sed -i "s/hostname='.*'/hostname='R2S'/g" package/base-files/files/bin/config_generate
+
 
 ##切换为samba4
 sed -i 's/luci-app-samba/luci-app-samba4/g' package/lean/autosamba/Makefile
