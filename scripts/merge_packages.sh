401c2de3780cb691f32a51f66b7d4345a5ea7142
00575 <75769819+00575@users.noreply.github.com>
2022-09-06 21:15:52 +0800

    Update merge_packages.sh

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -25,12 +25,7 @@ function merge_feed(){
     ./scripts/feeds install -a -p $1
 }
 
-rm -rf package/custom; mkdir package/custom
-merge_feed nas "https://github.com/linkease/nas-packages;master"
-merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
-rm -r package/feeds/nas_luci/luci-app-ddnsto
 merge_feed helloworld "https://github.com/klever1988/helloworld;tmp"
-#merge_package https://github.com/klever1988/openwrt-mos/trunk/luci-app-mosdns
 merge_package https://github.com/project-lede/luci-app-godproxy
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus
 merge_package https://github.com/kuoruan/openwrt-frp frp
